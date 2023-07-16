147cbe8b49af0dcf9cab8d9f25446a9a3abf7fc2
FelixCharm <105816617+FelixCharm@users.noreply.github.com>
2023-07-16 16:30:29 +0100

    Don't send bugged packets

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const world = require("./world.js");
 const config = require("./config.js");
 
 const mc = require("minecraft-protocol");
-const Chunk = require("prismarine-chunk")("1.18");
+const Chunk = require("prismarine-chunk")(config.version);
 
 const { analyse, checkIPs } = require("./analyse.js");
 
@@ -11,7 +11,8 @@ console.log("Starting honeypot...");
 
 const server = mc.createServer({
   "online-mode": true,
-  encryption: false,
+  encryption: true,
+  enforceSecureProfile: false,
   host: config.host,
   port: config.port,
   version: config.version,
@@ -22,45 +23,43 @@ const server = mc.createServer({
 });
 const mcData = require("minecraft-data")(server.version);
 
-const chunk = new Chunk();
+// const chunk = new Chunk();
 
-world.setup(chunk, mcData);
+// world.setup(chunk, mcData);
 
 console.log("Honeypot started");
 
 setInterval(checkIPs, 60000);
 
 server.on("login", function (client) {
-  const loginPacket = mcData.loginPacket;
+  // const loginPacket = mcData.loginPacket;
 
   analyse(client);
 
   // let player_brand; // Not used yet
 
-  client.registerChannel("minecraft:brand", ["string", []]);
-
   // client.on("minecraft:brand", (brand) => {
   //   player_brand = brand;
   // });
 
-  client.write("login", {
-    entityId: client.id,
-    isHardcore: false,
-    gameMode: 0,
-    previousGameMode: 1,
-    worldNames: loginPacket.worldNames,
-    dimensionCodec: loginPacket.dimensionCodec,
-    worldType: loginPacket.worldType,
-    worldName: "minecraft:overworld",
-    hashedSeed: [0, 0],
-    maxPlayers: 20,
-    viewDistance: 3,
-    simulationDistance: 3,
-    reducedDebugInfo: false,
-    enableRespawnScreen: true,
-    isDebug: false,
-    isFlat: true,
-  });
+  // client.write("login", {
+  //   entityId: client.id,
+  //   isHardcore: false,
+  //   gameMode: 0,
+  //   previousGameMode: 1,
+  //   worldNames: loginPacket.worldNames,
+  //   dimensionCodec: loginPacket.dimensionCodec,
+  //   worldType: loginPacket.worldType,
+  //   worldName: "minecraft:overworld",
+  //   hashedSeed: [0, 0],
+  //   maxPlayers: 20,
+  //   viewDistance: 3,
+  //   simulationDistance: 3,
+  //   reducedDebugInfo: false,
+  //   enableRespawnScreen: true,
+  //   isDebug: false,
+  //   isFlat: false
+  // });
 
   // for (let x = -5; x < 5; x++) {
   //   for (let z = -5; z < 5; z++) {
@@ -78,20 +77,21 @@ server.on("login", function (client) {
   // }
 
   // client.write("position", {
-  //   x: utils.getRandomInt(16),
-  //   y: 32,
-  //   z: utils.getRandomInt(16),
-  //   yaw: utils.getRandomInt(180),
+  //   x: 0,
+  //   y: 64,
+  //   z: 0,
+  //   yaw: 0,
   //   pitch: 0,
-  //   flags: 0x00,
+  //   flags: 0x00
   // });
 
-  client.writeChannel("minecraft:brand", "vanilla");
+  // client.registerChannel("minecraft:brand", ["string", []]);
+  // client.writeChannel("minecraft:brand", "vanilla");
 
-  client.on("chat", function (data) {
-    utils.broadcast(data.message, null, client.username, server);
-    console.log(client.username + ": " + data.message);
-  });
+  // client.on("chat", function (data) {
+  //   utils.broadcast(data.message, null, client.username, server);
+  //   console.log(client.username + ": " + data.message);
+  // });
 });
 
 server.on("error", function (error) {
